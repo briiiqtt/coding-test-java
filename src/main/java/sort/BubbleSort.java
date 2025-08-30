0fd4985c03e263bcf0260212842c156ea87de2cc
briiiqtt <briiiqtt@gmail.com>
2025-08-30 16:00:49 +0900

    selection

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -1,7 +1,7 @@
 package sort;
 
 public class BubbleSort {
-    public int[] execute(int[] arr) {
+    public int[] ascend(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
---
+++ src/main/java/sort/SelectionSort.java
@@ -0,0 +1,18 @@
+package sort;
+
+public class SelectionSort {
+    public int[] ascend(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        return arr;
+    }
+}
