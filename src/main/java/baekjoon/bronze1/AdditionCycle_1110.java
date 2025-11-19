8396699c55014f42ed1924b57a4c5763a43a33d4
briiiqtt <briiiqtt@gmail.com>
2025-11-19 10:40:43 +0900

    B-1110

---
+++ src/main/java/baekjoon/bronze1/AdditionCycle_1110.java
@@ -0,0 +1,23 @@
+package baekjoon.bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AdditionCycle_1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int x = n;
+        int cycle = 0;
+        do {
+            int a = x / 10;
+            int b = x % 10;
+            x = b * 10 + (a + b) % 10;
+            ++cycle;
+        } while (n != x);
+
+        System.out.println(cycle);
+    }
+}
