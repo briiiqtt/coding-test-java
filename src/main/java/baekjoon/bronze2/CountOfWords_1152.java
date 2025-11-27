3ff43ed3fc6e2e643ad29905a9a2a812e02dff05
briiiqtt <briiiqtt@gmail.com>
2025-11-27 22:14:20 +0900

    b1152_2

--- src/main/java/baekjoon/bronze2/CountOfWords_1152.java
+++ src/main/java/baekjoon/bronze2/CountOfWords_1152.java
@@ -3,12 +3,12 @@ package baekjoon.bronze2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class CountOfWords_1152 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine().trim();
-        if (line.isEmpty()) System.out.println(0);
-        else System.out.println(line.split(" ").length);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(st.countTokens());
     }
 }
