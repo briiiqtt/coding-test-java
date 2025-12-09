be3bac5ceff18d99aae507c3b235eba49836c40e
briiiqtt <briiiqtt@gmail.com>
2025-12-10 01:19:06 +0900

    baekjoon upstairing

--- src/main/java/baekjoon/silver5/B1181_SortingWords.java
+++ src/main/java/baekjoon/silver5/B1181_SortingWords.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class SortingWords_1181 {
+public class B1181_SortingWords {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
--- src/main/java/baekjoon/silver5/B1427_SortInside.java
+++ src/main/java/baekjoon/silver5/B1427_SortInside.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class SortInside_1427 {
+public class B1427_SortInside {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
