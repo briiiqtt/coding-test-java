ff3389b7d5745d68898b3bfc0970f3bb2c0fea5d
briiiqtt <briiiqtt@gmail.com>
2025-12-17 00:19:42 +0900

    simplified b3009

--- src/main/java/baekjoon/bronze3/B3009_FourthDot.java
+++ src/main/java/baekjoon/bronze3/B3009_FourthDot.java
@@ -8,26 +8,26 @@ import java.util.StringTokenizer;
 
 public class B3009_FourthDot {
     public static void main(String[] args) throws IOException {
-
-        //안다. 코드 나도 마음에 안든다.
-
         var br = new BufferedReader(new InputStreamReader(System.in));
-        var map1 = new HashMap<Integer, Boolean>();
-        var map2 = new HashMap<Integer, Boolean>();
+
+        int[] x = new int[3];
+        int[] y = new int[3];
 
         for (int i = 0; i < 3; i++) {
             var st = new StringTokenizer(br.readLine());
-            int key1 = Integer.parseInt(st.nextToken());
-            int key2 = Integer.parseInt(st.nextToken());
-            map1.put(key1, map1.get(key1) == null || map1.get(key1) == false);
-            map2.put(key2, map2.get(key2) == null || map2.get(key2) == false);
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (var e : map1.entrySet()) {
-            if (e.getValue()) System.out.print(e.getKey() + " ");
-        }
-        for (var e : map2.entrySet()) {
-            if (e.getValue()) System.out.print(e.getKey() + " ");
-        }
+        int newX = x[0];
+        int newY = y[0];
+
+        if (x[0] == x[1]) newX = x[2];
+        else if (x[0] == x[2]) newX = x[1];
+
+        if (y[0] == y[1]) newY = y[2];
+        else if (y[0] == y[2]) newY = y[1];
+
+        System.out.println(newX + " " + newY);
     }
 }
