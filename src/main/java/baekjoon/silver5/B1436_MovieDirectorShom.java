f21f2c52638158c633a61a298c812380d6d782ad
briiiqtt <briiiqtt@gmail.com>
2025-12-21 00:20:24 +0900

    b1436

--- src/main/java/baekjoon/silver5/B1436_MovieDirectorShom.java
+++ src/main/java/baekjoon/silver5/B1436_MovieDirectorShom.java
@@ -1,19 +1,24 @@
 package baekjoon.silver5;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class B1436_MovieDirectorShom {
-    public static void main(String[] args) {
-        int N = new Scanner(System.in).nextInt();
-        int cnt = 0;
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
 
-        int i;
-        for (i = 0; cnt < N; i++) {
-            if ((i + "").contains("666")) {
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        int i = 0;
+        while (cnt < N) {
+            ++i;
+            if (String.valueOf(i).contains("666")) {
                 ++cnt;
             }
         }
 
-        System.out.println(i - 1);
+        System.out.println(i);
     }
 }
