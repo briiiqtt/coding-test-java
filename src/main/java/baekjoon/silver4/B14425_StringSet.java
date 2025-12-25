23527bc2aaaf8c0f46307c30b98f87a4fcb0af17
briiiqtt <briiiqtt@gmail.com>
2025-12-26 01:03:07 +0900

    b14425

---
+++ src/main/java/baekjoon/silver4/B14425_StringSet.java
@@ -0,0 +1,33 @@
+package baekjoon.silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+/*
+자료구조: Set
+시간복잡도: O(N+M)
+공간복잡도: O(N)
+ */
+public class B14425_StringSet {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        var set = new HashSet<String>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < M; i++) {
+            if (set.contains(br.readLine())) cnt += 1;
+        }
+
+        System.out.println(cnt);
+    }
+}
