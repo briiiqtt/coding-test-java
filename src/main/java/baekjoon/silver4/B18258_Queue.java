b5dea0366c3372a53396dc07a0c4eb291e4bfcfb
briiiqtt <briiiqtt@gmail.com>
2025-12-30 17:20:36 +0900

    b18258

---
+++ src/main/java/baekjoon/silver4/B18258_Queue.java
@@ -0,0 +1,51 @@
+package baekjoon.silver4;
+/*
+자료구조: 큐
+시간복잡도: O(N)
+공간복잡도: O(N)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class B18258_Queue {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        var queue = new ArrayDeque<String>();
+        var sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            switch (line) {
+                case "pop": {
+                    sb.append(queue.isEmpty() ? -1 : queue.pop()).append("\n");
+                    break;
+                }
+                case "size": {
+                    sb.append(queue.size()).append("\n");
+                    break;
+                }
+                case "empty": {
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                }
+                case "front": {
+                    sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+                    break;
+                }
+                case "back": {
+                    sb.append(queue.isEmpty() ? -1 : queue.getLast()).append("\n");
+                    break;
+                }
+                default: {
+                    queue.addLast(line.split(" ")[1]);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
