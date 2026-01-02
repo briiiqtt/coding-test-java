adbdbb7491a2d3dd23d3b1b2bcefc66393de5274
briiiqtt <briiiqtt@gmail.com>
2026-01-02 15:31:50 +0900

    b15439

---
+++ src/main/java/baekjoon/bronze4/B15439_FashionOfVera.java
@@ -0,0 +1,17 @@
+package baekjoon.bronze4;
+/*
+NP2 = N*(N-1)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B15439_FashionOfVera {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N * (N - 1));
+    }
+}
