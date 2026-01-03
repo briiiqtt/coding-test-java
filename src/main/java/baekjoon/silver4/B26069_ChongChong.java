4a09341ea0aed92780a2b482b157c03ec6cf5fa7
briiiqtt <briiiqtt@gmail.com>
2026-01-03 14:52:02 +0900

    b26069

---
+++ src/main/java/baekjoon/silver4/B26069_ChongChong.java
@@ -0,0 +1,28 @@
+package baekjoon.silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class B26069_ChongChong {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        HashSet<String> set = new HashSet<>();
+        set.add("ChongChong");
+        for (int i = 0; i < N; i++) {
+            var st = new StringTokenizer(br.readLine());
+
+            String name1 = st.nextToken();
+            String name2 = st.nextToken();
+
+            if (set.contains(name1)) set.add(name2);
+            else if (set.contains(name2)) set.add(name1);
+        }
+
+        System.out.println(set.size());
+    }
+}
