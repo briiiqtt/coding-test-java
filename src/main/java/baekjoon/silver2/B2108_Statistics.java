272da99b2157aaa8c08f41136b58ad6e45b91f3a
briiiqtt <briiiqtt@gmail.com>
2026-01-03 14:51:45 +0900

    b2108

--- src/main/java/baekjoon/silver2/B2108_Statistics.java
+++ src/main/java/baekjoon/silver2/B2108_Statistics.java
@@ -1,7 +1,6 @@
 package baekjoon.silver2;
 /*
-누적합이 N/2를 넘는 순간이 중앙값이라는 생각을 못했다.
-다시 해보자.
+계수배열에서 계수를 누적시키고, 누적값이 N/2를 넘어서는 순간의 인덱스가 중간값임.
  */
 
 import java.io.BufferedReader;
@@ -15,25 +14,28 @@ public class B2108_Statistics {
         int N = Integer.parseInt(br.readLine());
 
         int sum = 0;
-        int[] arr = new int[N];
         int minVal = Integer.MAX_VALUE;
         int maxVal = Integer.MIN_VALUE;
 
         int[] counts = new int[8001];
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
-            arr[i] = num;
-            sum += num;
             counts[num + 4000] += 1;
             if (minVal > num) minVal = num;
             if (maxVal < num) maxVal = num;
+            sum += num;
         }
 
+        Integer mid = null;
+        int cntAcc = 0;
         int maxCnt = 0;
         for (int i = 0; i < counts.length; i++) {
-            if (counts[i] > maxCnt) {
+            int cnt = counts[i];
+            if (cnt > maxCnt) {
                 maxCnt = counts[i];
             }
+            cntAcc += cnt;
+            if (cntAcc > N / 2 && mid == null) mid = i - 4000;
         }
 
         ArrayList<Integer> list = new ArrayList<>();
@@ -43,9 +45,7 @@ public class B2108_Statistics {
 
         int mostNumerous = list.size() > 1 ? list.get(1) : list.get(0);
 
-        Arrays.sort(arr);
         float avg = (float) sum / N;
-        int mid = arr[arr.length / 2];
 
         System.out.println(Math.round(avg));
         System.out.println(mid);
