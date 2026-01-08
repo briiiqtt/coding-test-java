8784be7b5b5522351d284e74ecc1a02e8f7aa5b1
briiiqtt <briiiqtt@gmail.com>
2026-01-08 16:22:25 +0900

    random-marathon

---
+++ src/main/java/baekjoon/bronze4/B34073_Doro.java
@@ -0,0 +1,20 @@
+package baekjoon.bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B34073_Doro {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (st.hasMoreTokens()) {
+            if (sb.length() > 0) sb.append(" ");
+            sb.append(st.nextToken()).append("DORO");
+        }
+        System.out.println(sb);
+    }
+}
