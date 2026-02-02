d50746db6e9c82b48912a2e143adfd39e75d7562
briiiqtt <briiiqtt@gmail.com>
2026-02-03 00:09:11 +0900

    random marathon

---
+++ src/main/java/baekjoon/bronze1/B19843_SleepPattern.java
@@ -0,0 +1,51 @@
+package baekjoon.bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B19843_SleepPattern {
+
+    public static int getAbsoluteTime(String day, int time) {
+        switch (day) {
+            case "Mon":
+                return time;
+            case "Tue":
+                return 24 + time;
+            case "Wed":
+                return 48 + time;
+            case "Thu":
+                return 72 + time;
+            default:
+                return 96 + time;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+
+        int totalSlept = 0;
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            String day1 = st2.nextToken();
+            int time1 = Integer.parseInt(st2.nextToken());
+            String day2 = st2.nextToken();
+            int time2 = Integer.parseInt(st2.nextToken());
+
+            totalSlept += getAbsoluteTime(day2, time2) - getAbsoluteTime(day1, time1);
+        }
+
+        if (totalSlept + 48 < N) {
+            System.out.println(-1);
+        } else if (totalSlept > N) {
+            System.out.println(0);
+        } else {
+            System.out.println(N - totalSlept);
+        }
+    }
+}
---
+++ src/main/java/baekjoon/silver4/B9242_DisassembleBomb.java
@@ -0,0 +1,76 @@
+package baekjoon.silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class B9242_DisassembleBomb {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        /*
+         ***   * * * *** *** *** ***
+         * *   * * *   *   *   * *
+         * *   * *** *** *** *** ***
+         * *   *   * *     * *   * *
+         ***   *   * *** *** *** ***
+         */
+
+        String[] lines = new String[5];
+
+        for (int i = 0; i < 5; i++) {
+            lines[i] = br.readLine();
+        }
+
+        int codeLength = (lines[0].length() + 1) / 4;
+
+        StringBuilder codeSB = new StringBuilder();
+        for (int i = 0; i < codeLength; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < lines.length; j++) {
+                sb.append(lines[j].charAt(i * 4));
+                sb.append(lines[j].charAt(i * 4 + 1));
+                sb.append(lines[j].charAt(i * 4 + 2));
+            }
+
+            int c = parseCode(sb.toString());
+            if (c == -1) {
+                System.out.println("BOOM!!");
+                return;
+            }
+
+            codeSB.append(c);
+        }
+
+        int code = Integer.parseInt(codeSB.toString());
+        System.out.println(code % 6 == 0 ? "BEER!!" : "BOOM!!");
+    }
+
+    public static int parseCode(String code) {
+        switch (code) {
+            case "**** ** ** ****":
+                return 0;
+            case "  *  *  *  *  *":
+                return 1;
+            case "***  *****  ***":
+                return 2;
+            case "***  ****  ****":
+                return 3;
+            case "* ** ****  *  *":
+                return 4;
+            case "****  ***  ****":
+                return 5;
+            case "****  **** ****":
+                return 6;
+            case "***  *  *  *  *":
+                return 7;
+            case "**** ***** ****":
+                return 8;
+            case "**** ****  ****":
+                return 9;
+            default:
+                return -1;
+        }
+    }
+}
