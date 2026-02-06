0e060f2f38635a4dc0e3610a1a3d3d4c8aa97868
briiiqtt <briiiqtt@gmail.com>
2026-02-06 23:35:17 +0900

    b2052

---
+++ src/main/java/baekjoon/bronze1/B2052_Exponent.java
@@ -0,0 +1,20 @@
+package baekjoon.bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+
+public class B2052_Exponent {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        BigDecimal r = new BigDecimal(1);
+        BigDecimal a = new BigDecimal(2).pow(N);
+        r = r.divide(a);
+
+        System.out.println(r.toPlainString());
+    }
+}
+
