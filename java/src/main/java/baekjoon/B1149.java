d80cb1dd26245eafdadc43fbab6ea7683b7dea81
briiiqtt <briiiqtt@gmail.com>
2026-03-04 21:30:19 +0900

    b1149

---
+++ java/src/main/java/baekjoon/B1149.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1149 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[N][3];
+        st = new StringTokenizer(br.readLine());
+        dp[0][0] = Integer.parseInt(st.nextToken());
+        dp[0][1] = Integer.parseInt(st.nextToken());
+        dp[0][2] = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            dp[i][0] = Integer.parseInt(st.nextToken()) + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = Integer.parseInt(st.nextToken()) + Math.min(dp[i - 1][2], dp[i - 1][0]);
+            dp[i][2] = Integer.parseInt(st.nextToken()) + Math.min(dp[i - 1][0], dp[i - 1][1]);
+        }
+
+        int min = Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2]));
+        System.out.println(min);
+    }
+}
