5468d1b96238c856aa21889546ea485786392ab7
briiiqtt <briiiqtt@gmail.com>
2026-03-09 23:41:38 +0900

    b10953

---
+++ java/src/main/java/baekjoon/B10953.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10953 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
