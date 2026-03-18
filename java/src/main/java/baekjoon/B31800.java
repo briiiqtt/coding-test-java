c356304d63091e8d999571c2b5863aa164cbfcc2
briiiqtt <briiiqtt@gmail.com>
2026-03-18 23:52:25 +0900

    b31800

--- java/src/main/java/baekjoon/B31800.java
+++ java/src/main/java/baekjoon/B31800.java
@@ -6,40 +6,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B31800 {
-
     public static void main(String[] args) throws IOException {
-        /*
-        순수익 = 이익 - (기회비용 + 가격)
-        기회비용 = 자기제외최대이익 - 가격
-        순수익 = 이익 - ((자기제외최대이익 - 가격) + 가격)
-        순수익 = 이익 - 자기제외최대이익
-         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        br.readLine();
-
-        int max = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
         int[] profits = new int[N];
+        int profitMax = 0;
+        int profitSecondMax = 0;
         for (int i = 0; i < N; i++) {
-            int profit = Integer.parseInt(st.nextToken());
-            profits[i] = profit;
-            if (max < profit) {
-                secondMax = max;
-                max = profit;
-            } else if (secondMax < profit) {
-                secondMax = profit;
+            profits[i] = Integer.parseInt(st.nextToken());
+            if (profitMax < profits[i]) {
+                profitSecondMax = profitMax;
+                profitMax = profits[i];
+            } else if (profitSecondMax < profits[i]) {
+                profitSecondMax = profits[i];
             }
         }
-
-        StringBuffer sb = new StringBuffer();
+        st = new StringTokenizer(br.readLine());
+        /*int[] prices = new int[N];
         for (int i = 0; i < N; i++) {
-            if (sb.length() > 0) sb.append(" ");
-            int m = profits[i] == max ? secondMax : max;
-            sb.append(profits[i] - m);
+            prices[i] = Integer.parseInt(st.nextToken());
+        }*/
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int pureProfit = profits[i] == profitMax ? profits[i] - profitSecondMax : profits[i] - profitMax;
+            sb.append(pureProfit).append(" ");
         }
+
         System.out.println(sb);
     }
 }
