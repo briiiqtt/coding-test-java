6adca063339ce410598a8f1efd2cacd495e1a419
briiiqtt <briiiqtt@gmail.com>
2026-03-29 20:32:08 +0900

    b1000

---
+++ typescript/b1000.ts
@@ -0,0 +1,9 @@
+import * as fs from 'fs';
+
+// const input: string = fs.readFileSync('/dev/stdin').toString();
+const input: string = fs.readFileSync('./input.txt').toString();
+
+let sum: number = 0;
+input.split(' ').map(el => sum += parseInt(el));
+
+console.log(sum);
