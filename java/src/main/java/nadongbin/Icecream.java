dddef4a422c63643e9704c8529857db15f7b2312
briiiqtt <briiiqtt@gmail.com>
2026-04-09 16:46:59 +0900

    icecream

--- java/src/main/java/nadongbin/Icecream.java
+++ java/src/main/java/nadongbin/Icecream.java
@@ -8,19 +8,45 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Icecream {
+    static boolean[][] visit;
+    static int N, M;
+    static boolean[][] graph;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visit = new boolean[N][M];
 
-        boolean[][] graph = new boolean[N][M];
+        graph = new boolean[N][M];
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            char[] chars = br.readLine().toCharArray();
             for (int j = 0; j < M; j++) {
-                graph[i][j] = st.nextToken().equals("1");
+                graph[i][j] = chars[j] == '1';
             }
         }
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!visit[i][j] && !graph[i][j]) {
+                    dfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    static void dfs(int n, int m) {
+        if (visit[n][m]) return;
+        visit[n][m] = true;
+        if (n > 0 && !graph[n - 1][m]) dfs(n - 1, m);
+        if (n < N - 1 && !graph[n + 1][m]) dfs(n + 1, m);
+        if (m > 0 && !graph[n][m - 1]) dfs(n, m - 1);
+        if (m < M - 1 && !graph[n][m + 1]) dfs(n, m + 1);
     }
 }
